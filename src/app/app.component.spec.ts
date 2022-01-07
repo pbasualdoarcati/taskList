4b91f45a73a129b123043bb96c143d099b810753
pbasualdoarcati <pbasualdoarcati@hotmail.com>
2022-01-07 20:13:46 -0300

    Primer commit de aplicación basada en Angular para agregar y eliminar tareas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'taskList'`, () => {
+  it(`should have as title 'Task-List'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('taskList');
+    expect(app.title).toEqual('Task-List');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('taskList app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Task-List app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'taskList';
+  title = 'Lista de tareas en Angular';
+  list:any []= [];
+  addTask(item:string)
+  {
+    this.list.push({id:this.list.length, name:item});
+  }
+  removeTask(id:number)
+  {
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
