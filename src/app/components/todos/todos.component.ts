c87d8fc0933ee20551520d41940b4f0a8396f8d0
pbasualdoarcati <pbasualdoarcati@hotmail.com>
2022-01-08 17:19:37 -0300

    Mejora de primera aplicación

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {Todo} from './../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+
+  todos:Todo[]=[];
+  inputTodo:string="";
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  toggleDone(id: number){
+    this.todos.map((v, i)=> {
+      if (i == id) v.completed = !v.completed;
+      return v;
+    });
+  }
+  deleteTodo(id:number){
+    this.todos=this.todos.filter((v, i)=> i !== id  );
+  }
+  addTodo(){
+    this.todos.push({
+      content:this.inputTodo,
+      completed: false
+    });
+    this.inputTodo="";
+  }
+}
